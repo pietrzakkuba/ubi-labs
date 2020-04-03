37ee9309f53417c391127f0093bff4864fde2abf
pietrzakkuba <pietrzakkuba@yahoo.pl>
2020-04-03 13:08:48 +0200

    lab2 finish

---
+++ lab2-drugi-projekt/app/src/main/java/com/example/lab2second/LayoutSample.kt
@@ -0,0 +1,12 @@
+package com.example.lab2second
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutSample : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_sample)
+    }
+}
