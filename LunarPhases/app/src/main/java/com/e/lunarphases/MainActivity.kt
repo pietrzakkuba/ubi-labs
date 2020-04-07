eca45fb971777cc037da4568912168bd892408a8
pietrzakkuba <pietrzakkuba@yahoo.pl>
2020-04-07 15:16:17 +0200

    save settings

--- LunarPhases/app/src/main/java/com/e/lunarphases/MainActivity.kt
+++ LunarPhases/app/src/main/java/com/e/lunarphases/MainActivity.kt
@@ -7,20 +7,24 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
-    var hemisphere: Int? = null
-    var algorithm: Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val directory = File(this.filesDir, (R.string.settings_directory).toString())
+        directory.mkdirs()
+        val file = File(directory, "settings.txt")
+        if (!file.exists()) {
+            file.createNewFile()
+            file.writeText("2131230726\n2131230791")
+        }
+
         setContentView(R.layout.activity_main)
-
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
-
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_today, R.id.navigation_years, R.id.navigation_settings))
         setupActionBarWithNavController(navController, appBarConfiguration)
--- LunarPhases/app/src/main/java/com/e/lunarphases/ui/settings/SettingsFragment.kt
+++ LunarPhases/app/src/main/java/com/e/lunarphases/ui/settings/SettingsFragment.kt
@@ -4,28 +4,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.e.lunarphases.MainActivity
 import com.e.lunarphases.R
 import kotlinx.android.synthetic.main.fragment_settings.*
+import java.io.File
+
 
 class SettingsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val view: View = inflater.inflate(R.layout.fragment_settings, container, false)
-        return view
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        hemiGroup
-        NorthernHemisphereRadioButton.isChecked = true
-        SouthernHemisphereRadioButton
-
-        algGroup
-        alg0RadioButton
-        alg1RadioButton
-        alg2RadioButton
-        alg3RadioButton.isChecked = true
-
-
+        readSetup()
+        hemiGroup.setOnCheckedChangeListener { _, _ ->
+            writeSetup()
+        }
+        algGroup.setOnCheckedChangeListener { _, _ ->
+            writeSetup()
+        }
     }
+    private fun readSetup() {
+        val directory = File(context?.filesDir, (R.string.settings_directory).toString())
+        val file = File(directory, "settings.txt")
+        val data = file.readLines()
+        data[0].toInt().let { hemiGroup.check(it) }
+        data[1].toInt().let { algGroup.check(it) }
+    }
+    private fun writeSetup() {
+        val setup = hemiGroup.checkedRadioButtonId.toString() + "\n" + algGroup.checkedRadioButtonId.toString()
+        val directory = File(context?.filesDir, (R.string.settings_directory).toString())
+        val file = File(directory, "settings.txt")
+        file.writeText(setup)
+        Toast.makeText(activity, R.string.settings_toast, Toast.LENGTH_SHORT).show()
+    }
+//    hemiGroup
+//    NorthernHemisphereRadioButton.isChecked = true
+//    SouthernHemisphereRadioButton
+//
+//    algGroup
+//    alg0RadioButton
+//    alg1RadioButton
+//    alg2RadioButton
+//    alg3RadioButton.isChecked = true
+
 }
